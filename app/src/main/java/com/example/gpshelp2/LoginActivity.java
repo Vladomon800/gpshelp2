3d526fa55382a8761e1032a24a93b20a45cb96e4
vladomon800 <vlad-bolshakov-2016@mail.ru>
2023-04-06 18:38:25 +0300

    добавил базу данных и сделал регистрацию

---
+++ app/src/main/java/com/example/gpshelp2/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.example.gpshelp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email_login;
+    private EditText password_login;
+    private Button button_vhod;
+    private Button button_register;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email_login = findViewById(R.id.email_login);
+        password_login = findViewById(R.id.password_login);
+        button_vhod = findViewById(R.id.button_vhod);
+        button_register = findViewById(R.id.button_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_vhod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email_login.getText().toString().isEmpty() || password_login.getText().toString().isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                }else{
+                    mAuth.signInWithEmailAndPassword(email_login.getText().toString(), password_login.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    }else{
+                                        Toast.makeText(LoginActivity.this, "You have some errors", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+}
+
