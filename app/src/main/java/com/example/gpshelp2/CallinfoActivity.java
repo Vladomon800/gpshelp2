c625992236175e374323230b5d0c288b0aa7e651
benik <ben_andreasyan@mail.ru>
2023-05-15 19:43:20 +0300

    Создал класс для работы сервера и прописал логику для соединения с сервером (инфа отправляется с переменным успехом)

--- app/src/main/java/com/example/gpshelp2/CallinfoActivity.java
+++ app/src/main/java/com/example/gpshelp2/CallinfoActivity.java
@@ -1,19 +1,30 @@
 package com.example.gpshelp2;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
+import com.example.gpshelp2.server.LaptopServer;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class CallinfoActivity extends AppCompatActivity {
     EditText textadress, textinfo;
-    Button buttonsend, buttonback;
+    Button  buttonback;
+    private Button buttonsend = null;
+    private LaptopServer mServer = null;//экземпляр класса LaptopServer
+
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +42,49 @@ public class CallinfoActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        buttonsend.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onClick(View v) {
+                /* создаем объект для работы с сервером*/
+                mServer = new LaptopServer(); /* Открываем соединение. Открытие должно происходить в отдельном потоке от ui */
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            mServer.openConnection(); /* устанавливаем активные кнопки для отправки данных и закрытия соедиения. Все данные по обновлению интерфеса должны обрабатывается в Ui потоке, а так как мы сейчас находимся в отдельном потоке, нам необходимо вызвать метод runOnUiThread() */
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    buttonsend.setEnabled(true);
+                                }
+                            });
+                        } catch (Exception e) {
+                            Log.e(LOG_TAG, e.getMessage());
+                            mServer = null;
+                        }
+                    }
+                }).start();
+        if (mServer == null) {
+            Log.e(LOG_TAG, "Сервер не создан");
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    /* отправляем на сервер данные */
+                    mServer.sendData("Send text to server".getBytes());
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                }
+            }
+        }).start();
     }
-}
+});
+        }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mServer.closeConnection();
+    }
+        }
---
+++ app/src/main/java/com/example/gpshelp2/server/LaptopServer.java
@@ -0,0 +1,55 @@
+package com.example.gpshelp2.server;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class LaptopServer {
+    private static final String LOG_TAG = "myServerApp"; // ip адрес сервера, который принимает соединения
+    private String mServerName = "192.168.43.21"; // номер порта, на который сервер принимает соединения
+    private int mServerPort = 6789; // сокет, через которий приложения общается с сервером
+    private Socket mSocket = null;
+
+    public LaptopServer() {
+    }
+
+    public void openConnection() throws Exception {
+        closeConnection();
+        try {
+            mSocket = new Socket(mServerName, mServerPort);
+        } catch (IOException e) {
+            throw new Exception("Невозможно создать сокет: " + e.getMessage());
+        }
+    }
+
+    public void closeConnection() {
+        if (mSocket != null && !mSocket.isClosed()) {
+            try {
+                mSocket.close();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Невозможно закрыть сокет: " + e.getMessage());
+            } finally {
+                mSocket = null;
+            }
+        }
+        mSocket = null;
+    }
+    //функция отправления данных по сокету/////////////////////////////////////////
+//переменная data - данные, которые отправляем/////////////////////////////////
+    public void sendData(byte[] data) throws Exception{
+        if(mSocket == null || mSocket.isClosed()){
+            throw new Exception("Невозможно отправить данные. Сокет не создан или закрыт");
+        }
+        try {
+//отправка данных
+            mSocket.getOutputStream().write(data);
+            mSocket.getOutputStream().flush();
+        } catch (IOException e){
+            throw new Exception("Невозможно отправить данные: " + e.getMessage());
+        }
+    }
+    @Override
+    protected void finalize() throws Throwable { super.finalize(); closeConnection(); }
+}
+
